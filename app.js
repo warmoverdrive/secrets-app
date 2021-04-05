2b3ee8b4bb0ee64b7fedcfa45ab4e67cb86bfdcd
warmoverdrive <sweetsthebear@gmail.com>
2021-04-05 11:50:16 -0700

    Add mongoose + mongoDB. Set up crude user DB, registration+login, completely insecure and terrible to be iterated on

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 //jshint esversion:6
+
+//==============================================================//
+// Set-up
+//==============================================================//
+
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 
 const app = express();
@@ -8,17 +14,81 @@ app.use(express.static(`${__dirname}/public`));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+//==============================================================//
+// Mongoose/MongoDB setup
+//==============================================================//
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+const User = new mongoose.model("User", userSchema);
+
+//==============================================================//
+// Basic Routing
+//==============================================================//
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/login", (req, res) => {
-  res.render("login");
-});
+app
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-app.get("/register", (req, res) => {
-  res.render("register");
-});
+    User.findOne({ email: username }, (err, user) => {
+      if (err) {
+        log.error(err);
+      } else {
+        if (user) {
+          if (user.password === password) {
+            res.render("secrets");
+          } else {
+            console.log("incorrect password");
+            res.redirect("/login");
+          }
+        } else {
+          console.log("unknown email");
+          res.redirect("/login");
+        }
+      }
+    });
+  });
+
+app
+  .route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post((req, res) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password,
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
+
+//==============================================================//
+// Server startup
+//==============================================================//
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
