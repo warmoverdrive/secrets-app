0bdd62cc2a4f8d89c190024e17100cd4ca68f278
warmoverdrive <sweetsthebear@gmail.com>
2021-04-05 17:23:52 -0700

    set up google + facebook oAuth 2.0 using passport

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -41,18 +44,76 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 mongoose.set("useCreateIndex", true); // supress deprec warning
 
 const userSchema = new mongoose.Schema({
+  facebookId: String,
+  googleId: String,
   email: String,
   password: String,
 });
 // set up passport for local mongoose
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 // configure passport for Mongoose
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+//==============================================================//
+// Set up OAuth2.0 Strategies
+//==============================================================//
+
+// set up google oauth2
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      //userProfileURL: "https://www.googleapps.com/oauth2/v3/userinfo",
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      User.findOrCreate(
+        { googleId: profile.id, username: profile.emails[0].value },
+        function (err, user) {
+          return cb(err, user);
+        }
+      );
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: "http://localhost:3000/auth/facebook/secrets",
+      //passReqToCallback: true,
+      profileFields: ["id", "email"],
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(profile);
+      User.findOrCreate(
+        {
+          facebookId: profile.id,
+          username: profile.emails[0].value,
+        },
+        (err, user) => {
+          return cb(err, user);
+        }
+      );
+    }
+  )
+);
 
 //==============================================================//
 // Basic Routing
@@ -61,6 +122,23 @@ passport.deserializeUser(User.deserializeUser());
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.route("/secrets").get((req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.route("/logout").get((req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+//==============================================================//
+// Manual Login/Register Routes
+//==============================================================//
 
 app
   .route("/login")
@@ -106,18 +184,36 @@ app
     );
   });
 
-app.route("/secrets").get((req, res) => {
-  if (req.isAuthenticated()) {
-    res.render("secrets");
-  } else {
-    res.redirect("/login");
-  }
-});
+//==============================================================//
+// Google OAuth 2.0
+//==============================================================//
 
-app.route("/logout").get((req, res) => {
-  req.logout();
-  res.redirect("/");
-});
+app.get("/auth/google", passport.authenticate("google", { scope: ["email"] }));
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    res.redirect("/secrets");
+  }
+);
+
+//==============================================================//
+// Facebook OAuth
+//==============================================================//
+
+app.get(
+  "/auth/facebook",
+  passport.authenticate("facebook", { scope: ["email"] })
+);
+
+app.get(
+  "/auth/facebook/secrets",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  (req, res) => {
+    res.redirect("/secrets");
+  }
+);
 
 //==============================================================//
 // Server startup
